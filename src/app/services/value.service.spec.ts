1d9e0b99ab0bb710c583cab19187b42a932f3e2d
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-04-02 11:00:34 -0300

    feat: add first service and test some methods

---
+++ src/app/services/value.service.spec.ts
@@ -0,0 +1,55 @@
+import { ValueService } from './value.service';
+
+fdescribe('ValueService', () => {
+  let service: ValueService;
+
+  beforeEach(() => {
+    service = new ValueService();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('Tests for getValue', () => {
+    it('should return "my value"', () => {
+      expect(service.getValue()).toBe('my value');
+    });
+  });
+
+  describe('Tests for setValue', () => {
+    it('should change the value', () => {
+      expect(service.getValue()).toBe('my value');
+
+      service.setValue('changed value');
+
+      expect(service.getValue()).toBe('changed value');
+    });
+  });
+
+  describe('Tests for getPromiseValue', () => {
+    it('should return "promise value" from promise(then)', (doneFn) => {
+      service.getPromiseValue().then((value) => {
+        expect(value).toBe('promise value');
+
+        doneFn();
+      });
+    });
+
+    it('should return "promise value" from promise(async/await)', async () => {
+      const rta = await service.getPromiseValue();
+
+      expect(rta).toBe('promise value');
+    });
+  });
+
+  describe('Tests for getObservable', () => {
+    it('should return "observable value"', (doneFn) => {
+      service.getObservable().subscribe((value) => {
+        expect(value).toBe('observable value');
+
+        doneFn();
+      });
+    });
+  });
+});
