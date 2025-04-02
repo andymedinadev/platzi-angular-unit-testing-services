07b086bf70c181752fa5cc2c93444fc888319e65
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-04-02 10:37:02 -0300

    feat: add mocha reporter to Karma configuration and improve tests

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,42 +1,46 @@
 import { Calculator } from './calculator';
 
 describe('Test for Calculator', () => {
-  it('#multiply should return a nine', () => {
-    // Arrange
-    const calculator = new Calculator();
+  describe('Tests for multiply method', () => {
+    it('should return a nine', () => {
+      // Arrange
+      const calculator = new Calculator();
 
-    // Act
-    const rta = calculator.multiply(3, 3);
+      // Act
+      const rta = calculator.multiply(3, 3);
 
-    // Assert
-    expect(rta).toEqual(9);
+      // Assert
+      expect(rta).toEqual(9);
+    });
+
+    it('should return a four', () => {
+      // Arrange
+      const calculator = new Calculator();
+
+      // Act
+      const rta = calculator.multiply(1, 4);
+
+      // Assert
+      expect(rta).toEqual(4);
+    });
   });
 
-  it('#multiply should return a four', () => {
-    // Arrange
-    const calculator = new Calculator();
+  describe('Tests for divide method', () => {
+    it('should return some numbers', () => {
+      // Arrange
+      const calculator = new Calculator();
 
-    // Act
-    const rta = calculator.multiply(1, 4);
+      // Act and Assert
+      expect(calculator.divide(6, 3)).toEqual(2);
+      expect(calculator.divide(5, 2)).toEqual(2.5);
+    });
 
-    // Assert
-    expect(rta).toEqual(4);
-  });
+    it('divide for zero', () => {
+      const calculator = new Calculator();
 
-  it('#divide should return some numbers', () => {
-    // Arrange
-    const calculator = new Calculator();
-
-    // Act and Assert
-    expect(calculator.divide(6, 3)).toEqual(2);
-    expect(calculator.divide(5, 2)).toEqual(2.5);
-  });
-
-  it('#divide for zero', () => {
-    const calculator = new Calculator();
-
-    expect(calculator.divide(2, 0)).toBeNull();
-    expect(calculator.divide(555245252, 0)).toBeNull();
+      expect(calculator.divide(2, 0)).toBeNull();
+      expect(calculator.divide(555245252, 0)).toBeNull();
+    });
   });
 
   it('test matchers', () => {
