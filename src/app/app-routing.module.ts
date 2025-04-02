3f45f66b23bd963bcc694f8234601362bfd6c72b
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-04-02 16:13:24 -0300

    chore: add Products component and service to start testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductsComponent } from './components/products/products.component';
+
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
