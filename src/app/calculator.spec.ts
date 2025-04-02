e628f676e7aae3ca676cb00f3274864ed2f2f1f1
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-04-02 10:29:22 -0300

    feat: add more tests

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -11,4 +11,48 @@ describe('Test for Calculator', () => {
     // Assert
     expect(rta).toEqual(9);
   });
+
+  it('#multiply should return a four', () => {
+    // Arrange
+    const calculator = new Calculator();
+
+    // Act
+    const rta = calculator.multiply(1, 4);
+
+    // Assert
+    expect(rta).toEqual(4);
+  });
+
+  it('#divide should return some numbers', () => {
+    // Arrange
+    const calculator = new Calculator();
+
+    // Act and Assert
+    expect(calculator.divide(6, 3)).toEqual(2);
+    expect(calculator.divide(5, 2)).toEqual(2.5);
+  });
+
+  it('#divide for zero', () => {
+    const calculator = new Calculator();
+
+    expect(calculator.divide(2, 0)).toBeNull();
+    expect(calculator.divide(555245252, 0)).toBeNull();
+  });
+
+  it('test matchers', () => {
+    let name = 'platzi';
+    let name2;
+
+    expect(name).toBeDefined();
+    expect(name2).toBeUndefined();
+
+    expect(1 + 3 === 4).toBeTruthy();
+    expect(1 + 1 === 3).toBeFalsy();
+
+    expect(5).toBeLessThan(10);
+    expect(20).toBeGreaterThan(10);
+
+    expect('123456').toMatch(/123/);
+    expect(['apples', 'oranges', 'pears']).toContain('oranges');
+  });
 });
